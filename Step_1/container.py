7261a61a0a36b23b5fda126e120dc0342fedd2ef
HyunSB <52453601+hyunseungbin9408@users.noreply.github.com>
2020-07-20 15:18:48 +0900

    Add files via upload

---
+++ Step_1/container.py
@@ -0,0 +1,10 @@
+print(type('hyun'))
+name = 'hyun'
+print(name)
+print(type(['hyun', 'hak', 'ahah']))
+names = ['hyun', 'hak', 'ahah']
+print(names)
+print(names[2])
+hyun = ['programmer', 'seoul', 27, False]
+hyun[1] = 'jeju'
+print(hyun)
---
+++ Step_1/multipication.py
@@ -0,0 +1,6 @@
+num = (int(input()))
+a,b,c = map(int, input())
+print(num * c)
+print(num * b)
+print(num * a)
+print((num * (a*100))+(num * (b*10))+(num*c))
