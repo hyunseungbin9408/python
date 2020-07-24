b362dabcd39e73a705d8273d6e97957469bf5709
HyunSB <52453601+hyunseungbin9408@users.noreply.github.com>
2020-07-24 09:51:07 +0900

    Delete 10952.py

--- Step_4/10952.py
+++
@@ -1,6 +0,0 @@
-num,num2 = map(int,input().split())
-print (num + num2)
-while num != 0 and num2 != 0:
-    num,num2 = map(int,input().split())
-    if num != 0 and num2 != 0:
-        print(num + num2)
