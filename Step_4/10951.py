30601aa8092217730235333eb41af6f23089d484
HyunSB <52453601+hyunseungbin9408@users.noreply.github.com>
2020-07-24 09:51:01 +0900

    Delete 10951.py

--- Step_4/10951.py
+++
@@ -1,9 +0,0 @@
-num = 1
-num2 = 0
-while True:
-    try:
-        if 0 < num and num2 < 10:
-            num,num2 = map(int,input().split())
-            print (num + num2)
-    except: 
-        break
